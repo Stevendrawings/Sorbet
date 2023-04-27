d880927fa7d4415d7a07e2cbaed341f5ae9878b2
stevendrawings <stevenchauvets@gmail.com>
2023-04-27 14:30:47 +0200

    Modification du focus et la partie tags

--- js/app.js
+++ js/app.js
@@ -7,19 +7,24 @@ var inputHideTags = document.querySelector('#inputTags');
 var tabsTxt = texte.innerHTML.toLowerCase().split(' ');
 var tableau = Array(tabsTxt.length)
 
-function eventListetterFocus(){
-    parentFocus.style.borderColor = "#e84118";
-    parentFocus.style.boxShadow = "rgba(255, 0, 0, 0.353)";
-}
-
 function hundlehideTexte(){
     var valeur = inputHideTags.value   
     var index = retrieve(tabsTxt, valeur);
         if(tabsTxt.at(index) == valeur){
+            noticeAlert.textContent = "Vous avez entré le mot '" 
+            + valeur + "' dans le tableau.";
+            noticeAlert.style.color = "rgb(97 243 74)";
+            parentFocus.style.border = "2px solid rgb(97 243 74)";
+            parentFocus.style.boxShadow = "rgb(67 255 98 / 25%) 0px 0px 0px 0.2rem";
             if(tableau.indexOf(valeur) === -1){
                 console.log('Le nouveau tableau est : ' 
                 + createTags(valeur));
             } if(tableau.indexOf(valeur) > -1){
+                noticeAlert.textContent = "Vous avez deja entré le mot '" 
+                + valeur + "' dans le tableau !";
+                noticeAlert.style.color = "#00cbff";
+                parentFocus.style.border = "2px solid #00cbff";
+                parentFocus.style.boxShadow = "rgb(0 196 255 / 25%) 0px 0px 0px 0.2rem";
                 console.log(valeur 
                 + ' existe déjà dans le tableau.');
                 return false
@@ -27,9 +32,10 @@ function hundlehideTexte(){
             tableau.splice(index, 1, tabsTxt.at(index))
             console.log(tableau)
         } if(tabsTxt.at(index) != valeur){
+            noticeAlert.style.color = "red";
+            noticeAlert.textContent = "Vous avez entré aucun mot dans le champ !";
             parentFocus.style.border = "2px solid red";
             parentFocus.style.boxShadow = "0 0 0 0.2rem rgb(255 0 0 / 25%)";
-            parentFocus.style.backgroundColor = "#ffebeb";
             console.log(tableau.splice(0, 0))
             return false
         }
@@ -40,10 +46,10 @@ function createTags(paramText){
     var newContent = document.createTextNode(paramText);
         newDiv.appendChild(newContent);
         parentTag.appendChild(newDiv)
+        newDiv.className = "sorbetTags";
         return paramText;
     }
 
 function retrieve(arr, val){ return arr.indexOf(val) }
 
 btnHundle.addEventListener('click', hundlehideTexte)
-inputHideTags.addEventListener('click', eventListetterFocus)
